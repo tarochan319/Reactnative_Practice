774a2c8835239b26d2e38238bb080088a0a66882
Yutaro Kodera <tarochan319@gmail.com>
2022-10-04 17:45:09 +0900

    Restart

--- App.js
+++ App.js
@@ -2,17 +2,25 @@
 // ・画面遷移の実装（下記参照）
 //https://fintan-contents.github.io/mobile-app-crib-notes/react-native/learn/basic-concepts/react-navigation-basics/
 
-import "react-native-gesture-handler";
-import * as React from "react"; //必要
-import { StyleSheet, View, Text } from "react-native"; //コンポーネント読み込み
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import TitleScreen from "../oyaji_project/src/screens/TitleScreen";
-import TopScreen from "../oyaji_project/src/screens/TopScreen";
-import MeigenScreen from "../oyaji_project/src/screens/MeigenScreen";
-import ToDoScreen from "../oyaji_project/src/screens/ToDoScreen";
-import CompleteScreen from "../oyaji_project/src/screens/CompleteScreen";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Test from "./Component/test";
 
-const Stack = createStackNavigator();
+export default function App() {
+    return(
+        <View style={StyleSheet.container}>
+            <Text>タイトル</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
 
 
---
+++ Component/navigation/index.js
@@ -0,0 +1,28 @@
+
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import TitleScreen from "../screens/TitleScreen";
+// import TopScreen from "../screens/TopScreen";
+// import MeigenScreen from "../screens/MeigenScreen";
+// import ToDoScreen from "../screens/ToDoScreen";
+// import CompleteScreen from "../screens/CompleteScreen";
+
+// const RootStack = createStackNavigator();
+
+// const RootStackScreen = () => {
+//   return (
+//     <NavigationContainer> 
+//       <RootStack.Navigator>
+//         {/* 一番上のページが初期表示される。 */}
+//         <RootStack.Screen name="Title" component={TitleScreen} />
+//         <RootStack.Screen name="Top" component={TopScreen} />
+//         <RootStack.Screen name="Meigen" component={MeigenScreen} />
+//         <RootStack.Screen name="ToDo" component={ToDoScreen} />
+//         <RootStack.Screen name="Complete" component={CompleteScreen} />
+//       </RootStack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default RootStackScreen;
