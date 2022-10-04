b26f407c2e73e65ce771b5a8765da17a47d6b379
Yutaro Kodera <tarochan319@gmail.com>
2022-10-04 21:52:09 +0900

    New_Project

--- App.js
+++ App.js
@@ -1,26 +1,41 @@
-// タスク
-// ・画面遷移の実装（下記参照）
-//https://fintan-contents.github.io/mobile-app-crib-notes/react-native/learn/basic-concepts/react-navigation-basics/
-
-import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Test from "./Component/test";
+import React from "react";
+import { StyleSheet, Tesxt, View } from "react-native";
 
 export default function App() {
-    return(
-        <View style={StyleSheet.container}>
-            <Text>タイトル</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <View style={styles.title}></View>
+        <View style={styles.image}></View>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create ({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  box: {
+    height: 100,
+    width: "100%",
+    borderColor: "red",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+
+  title: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+  },
+
+  image: {
+    width: 100,
+    backgroundColor: "black",
+  },
 });
-
-
