6fcecbe943ddd364ee732c369de40e262794e9af
Yutaro Kodera <tarochan319@gmail.com>
2022-10-05 11:07:38 +0900

    Screen not displayed

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
 import CampNews from "./Component/CampNews";
+import Constants from "expo-constants";
+import axios from "axios";
 
-// ▼タスク
-// API取得から！
-// https://youtu.be/28CvBaSsoEI
+const URI = `https://newsapi.org/v2/top-headlines?country=jp&category=entertainment&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [news, setNews] = useState([]);
+
+  // 1.画面表示でuseEffectが動く
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  // 2.axiosを使ってAPI実行
+  const getNews = async () => {
+    const response = await axios.get(URI);
+    setNews(response.data.articles);
+  };
+
   return (
     <View style={styles.container}>
-      {/* ▼自作コンポーネントの呼び出し */}
-      <CampNews imageuri="" title="てすと" subtext="てすと"></CampNews>
+      <FlatList>
+        data={news}
+        renderItem ={" "}
+        {({ item }) => (
+          // ▼CampNewsは自作コンポーネント
+          <CampNews>
+            imageuri= {item.urIToImage}
+            title= {item.title}
+            subtext= {item.publishedAt}
+          </CampNews>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      </FlatList>
     </View>
   );
 }
@@ -18,7 +43,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
